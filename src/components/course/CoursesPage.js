c3d98ca78bb54a55fb0d636984d0e93132ba4f49
KZ <zhiping72@gmail.com>
2016-08-17 22:47:40 +0200

    Course Page: form added

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,10 +1,41 @@
 import React from 'react';
 
-class CoursesPage extends React.Component{
-  render(){
-    return(
+class CoursesPage extends React.Component {
+
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+      course: {title: ''}
+    };
+
+    this._onTitleChange = this._onTitleChange.bind(this);
+    this._onClickSave = this._onClickSave.bind(this);
+  }
+
+  _onTitleChange(event) {
+    const course = this.state.course;
+    course.title = event.target.value;
+    this.setState({course: course});
+  }
+
+  _onClickSave() {
+    alert(`Saving ${this.state.course.title}`);
+  }
+
+  render() {
+    return (
       <div>
-        <h2>Courses</h2>
+        <h1>Courses</h1>
+        <h2>Add Course</h2>
+
+        <input type="text"
+               value={this.state.course.title}
+               onChange={this._onTitleChange}/>
+
+        <input type="submit"
+               value="Save"
+               onClick={this._onClickSave}/>
       </div>
     )
   }
